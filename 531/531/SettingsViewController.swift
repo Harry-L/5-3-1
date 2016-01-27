64dc21c354419a4d2316983b6c3d33ad438c4d14
Harry-L <harryliu0@hotmail.com>
2016-01-26 20:36:26 -0500

    Basic working app

--- 531/531/SettingsViewController.swift
+++ 531/531/SettingsViewController.swift
@@ -66,7 +66,6 @@ class SettingsViewController: UITableViewController {
     
     
     override func viewDidLoad() {
-        print("loaded")
         super.viewDidLoad()
         
         loadData()
